d528f76b31c6f482a759d2fae1000ffa6a44418d
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-03-11 18:52:11 +0000

    more static files changes moved static files to a more sensible location added logo added credit for background

--- package_manager/settings.py
+++ package_manager/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STYLE_DIR = os.path.join(BASE_DIR, 'styles')
+STYLE_DIR = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [STYLE_DIR, ]
 
