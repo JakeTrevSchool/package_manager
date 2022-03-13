3bc180f6c417278f72a0688068d36a0feca1aa81
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-03-13 14:31:24 +0000

    fixed template inheritance

--- package_manager/urls.py
+++ package_manager/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include(registration.backends.simple.urls))
+    path('accounts/', include('registration.backends.simple.urls')),
     path('', include('manager.urls')),
 ]
