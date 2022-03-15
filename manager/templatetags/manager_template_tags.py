aeb833b9dc7793177e935151c8612592771d850e
“Constantinos <2099999z@student.gla.ac.uk>
2022-03-15 11:50:42 +0000

    Avatars on base changes

--- manager/templatetags/manager_template_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from manager.models import UserProfile
-
-register = template.Library()
-
-@register.inclusion_tag('manager/UserProfile.html')
-def get_profile_picture():
-	return UserProfile.objects.get.all()
